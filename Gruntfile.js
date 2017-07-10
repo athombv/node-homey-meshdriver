1c31bb2e6de25fffec7658eaedbc963d60a37ca7
Emile Nijssen <emile@athom.nl>
2017-07-10 20:15:42 +0200

    update docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function(grunt) {
 	        dist: {
 	            src: ['lib/**/*.js'],
 	            options: {
+		            configure: 'jsdoc.json',
 	                destination: 'docs'
 	            }
 	        }
