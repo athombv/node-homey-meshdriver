2778248ddbe1bd42407c67599d6285a458b67c31
Emile Nijssen <emile@athom.nl>
2017-07-10 20:25:32 +0200

    update example

---
+++ README.md
@@ -0,0 +1,18 @@
+# homey-meshdriver
+
+## Introduction
+This module is used to make the creation of Z-Wave & ZigBee apps easier.
+
+It is essentially a map-tool from Homey-capabilities to Z-Wave Command Classes or ZigBee endpoints.
+
+## Installation
+
+```bash
+$ npm install homey-meshdriver
+```
+
+## Usage
+See [examples/fibaroplug.js]()
+
+## Docs
+See [https://athombv.github.io/node-homey-meshdriver]()
---
+++ examples/fibaroplug.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Homey = require('homey');
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+
+class FibaroPlugDevice extends ZwaveDevice {
+	
+	onMeshInit() {
+		
+		// enable debugging
+		this.setDebug( true );
+		
+		// print the node's info to the console
+		this.printNode();
+		
+		// register the `onoff` capability with COMMAND_CLASS_SWITCH_BINARY
+		this.registerCapability('onoff', 'SWITCH_BINARY', {
+			getOpts: {
+				getOnStart: true, // get the initial value on app start
+				pollInterval: 'poll_interval' // maps to device settings
+				// getOnWakeUp: true, // only useful for battery devices
+			}
+		});
+		
+		// register a settings parser
+		this.registerSetting('always_on', value => {			
+			return new Buffer([ (value === true) ? 0 : 1 ])
+		});
+		
+		// register a report listener
+		this.registerReportListener('SWITCH_BINARY', 'SWITCH_BINARY_REPORT', ( rawReport, parsedReport ) => {
+			console.log('registerReportListener', rawReport, parsedReport);
+		});
+		
+	}
+	
+	// Overwrite the onSettings method, and change the Promise result
+	onSettings( oldSettings, newSettings, changedKeysArr ) {
+		return super.onSettings(oldSettings, newSettings, changedKeysArr)
+			.then( res => {
+				return 'Success!';
+			})
+			.catch( err => {
+				return 'Error!';
+			})
+	}
+	
+}
+
+module.exports = FibaroPlugDevice;
