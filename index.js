e85bffc10b9f0cdc5d15b972a790cc915c4a41ef
Robin Bolscher <RobinBol@users.noreply.github.com>
2017-10-09 11:22:36 +0200

    Add ZwaveLockDevice (#31)
    * Add basic ZwaveLockDevice
    
    * Fix typo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports.ZwaveDevice = require('./lib/zwave/ZwaveDevice.js');
+module.exports.ZwaveLockDevice = require('./lib/zwave/ZwaveLockDevice.js');
+
 module.exports.ZigBeeDevice = require('./lib/zigbee/ZigBeeDevice.js');
 module.exports.ZigBeeLightDevice = require('./lib/zigbee/ZigBeeLightDevice.js');
 module.exports.ZigBeeXYLightDevice = require('./lib/zigbee/ZigBeeXYLightDevice.js');
