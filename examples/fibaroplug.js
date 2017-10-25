d664436b3f7adb63a88bebf88c6567988bb63318
Robin Bolscher <robinbolscher@gmail.com>
2017-10-25 14:48:06 +0200

    Remove reference to getOnWakeUp

--- examples/fibaroplug.js
+++ examples/fibaroplug.js
@@ -19,7 +19,6 @@ class FibaroPlugDevice extends ZwaveDevice {
 				getOnStart: true, // get the initial value on app start (only use for non-battery devices)
 				pollInterval: 'poll_interval' // maps to device settings
 				// getOnOnline: true, // use only for battery devices
-				// getOnWakeUp: true, // only useful for battery devices
 			},
 			getParserV3: ( value, opts ) => {
 				return {};
